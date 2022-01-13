6112cbaedfc4fd3cb5c100e2f076cf92bfb50a25
tessam30 <tim.essam@gmail.com>
2022-01-13 14:04:20 -0500

    review:added in mechanism names

--- Scripts/01_zambia-target-munge-table.R
+++ Scripts/01_zambia-target-munge-table.R
@@ -27,22 +27,35 @@ load_secrets() #glamr function to load credentials for google drive
 # IMPORT ------------------------------------------------------------------
 
 df <-  si_path() %>% # routes R to my data folder (use glamr::set_paths() to set up)
-  return_latest("Zambia") %>% # returns the latest file in directory based on the pattern
+  return_latest(pattern = "Site_IM_FY19-22.*\\Zambia.zip") %>% # returns the latest file in directory based on the pattern
   read_msd() #reads the MSD
         
 # MUNGE -------------------------------------------------------------------
 
 df_target <- df %>% 
+  clean_agency() %>% #Tidy up agency names so we don't have to carry along HHS
   filter(fiscal_year %in% c(2021, 2022),
-         fundingagency %in% c("HHS/CDC", "USAID"),
+         fundingagency %in% c("CDC", "USAID"),
          standardizeddisaggregate == "Total Numerator") %>% 
-  group_by(mech_code, primepartner, fundingagency, indicator, fiscal_year) %>% 
+  group_by(mech_code, mech_name, primepartner, fundingagency, indicator, fiscal_year) %>% 
   summarise(across(c(targets), sum, na.rm = TRUE)) %>% 
   ungroup() %>% 
   pivot_wider(names_from = fiscal_year, values_from = targets) %>% 
   rename(COP21_target = `2021`,
          COP22_target = `2022`) %>% 
   arrange(desc(fundingagency)) # remove/keep indicators with NA's/zeroes for FY21/FY22?
+
+# Clean up mechanism names to shorten text
+df_target <-
+  df_target %>%
+  mutate(mech_name = case_when(
+    str_detect(mech_name, "DISCOVER-H") ~ "DISCOVER-H",
+    str_detect(mech_name, "Stop GBV") ~ "Stop GBV",
+    str_detect(mech_name, "Z-CHPP") ~ "Z-CHPP",
+    TRUE ~ mech_name), #Clean up the OPU names below so openxlxs writes ok
+    mech_name = str_remove_all(mech_name, "\\[Placeholder|\\]|-|[[:digit:]]") %>% trimws(.)
+  ) %>%
+  relocate(mech_name, .before = mech_code)
 
 
 #Check Pano Data Table - Data Validation Step
@@ -57,22 +70,35 @@ df_target <- df %>%
 
 #USING OPEN_XLSX()
 
+#+1 extra rows for header
+tot_rows <- nrow(df_target) + 1
+tot_cols <- ncol(df_target)
+
 #create the excel workbook
 wb <- createWorkbook()
 
 #add the blank sheet
-addWorksheet(wb, "Sheet 1")
+addWorksheet(wb, "Zambia COP Targets by IM")
 
 #define style for headers
-hs <- createStyle(fontColour = "#ffffff", fgFill = "#4F80BD",
-                  halign = "center", valign = "center", textDecoration = "Bold",
-                  border = "TopBottomLeftRight")
+hs <- createStyle(fgFill = "#EAEAEA", halign = "CENTER", textDecoration = "Bold",
+                  border = "Bottom", fontColour = "#414042")
+
+#Header row height = 32
+setRowHeights(wb, 1, rows = 1, heights = 32)
+
+#set column widths
+setColWidths(wb, 1, cols = 1:tot_cols, widths = "auto", hidden = rep(FALSE, length(cols)))
 
 #write data to the sheet and add style
-writeData(wb, 1, df_target, borders = "rows", headerStyle = hs)
+writeData(wb, 1, df_target, headerStyle = hs, borders = "rows")
 
 #adding filters to the columns mech_code, prime_partner, funding_agency, and indicator
-addFilter(wb, 1, row = 1, cols = 1:4)
+addFilter(wb, 1, row = 1, cols = 1:5)
+
+#adding commas for targets column
+nbr <- createStyle(numFmt = "#,##0")
+addStyle(wb, 1, style = nbr, rows = 2:tot_rows, cols = 6:7, gridExpand = T, stack = T)
 
 #save workbook and write to dataout folder
 saveWorkbook(wb, file = "Dataout/zambia-targets-fy21-fy22.xlsx", overwrite = TRUE)
